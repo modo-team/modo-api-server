a6cfae766be26205df4bb379c21172d3e9280fa1
diocto <duhee74@naver.com>
2024-08-18 15:23:11 +0900

    goal team 상호작용 API 엔드포인트 정의

--- src/main/java/com/modo/modoapiserver/controller/goalteam/GoalTeamActionController.java
+++ src/main/java/com/modo/modoapiserver/controller/goalteam/GoalTeamActionController.java
@@ -19,14 +19,46 @@ import org.springframework.web.bind.annotation.*;
 public class GoalTeamActionController {
     /// # 공통 상호작용
     /// 팀 좋아요 API
+    @PostMapping("/{teamId}/like")
+    public ResponseEntity<?> likeTeam(@PathVariable("teamId") Long teamId) {
+        return ResponseEntity.ok().build();
+    }
+
+    /// 팀 좋아요 취소 API
+    @DeleteMapping("/{teamId}/like")
+    public ResponseEntity<?> unlikeTeam(@PathVariable("teamId") Long teamId) {
+        return ResponseEntity.ok().build();
+    }
 
     /// # 참가자 상호작용
     /// 팀 나가기 API
+    @DeleteMapping("/{teamId}/member")
+    public ResponseEntity<?> leaveTeam(@PathVariable("teamId") Long teamId) {
+        return ResponseEntity.ok().build();
+    }
+
     /// 팀 참가신청 API
+    @PostMapping("/{teamId}/apply")
+    public ResponseEntity<?> applyTeam(@PathVariable("teamId") Long teamId) {
+        return ResponseEntity.ok().build();
+    }
+
     /// 팀 참가신청 취소 API
+    @DeleteMapping("/{teamId}/apply")
+    public ResponseEntity<?> cancelApplyTeam(@PathVariable("teamId") Long teamId) {
+        return ResponseEntity.ok().build();
+    }
 
     /// # 팀장 상호작용
     /// 팀 참가신청 수락 API
-    /// 팀 참가신청 거절 API
+    @PostMapping("/{teamId}/apply/{userId}/accept")
+    public ResponseEntity<?> acceptApplyTeam(@PathVariable("teamId") Long teamId, @PathVariable("userId") Long userId) {
+        return ResponseEntity.ok().build();
+    }
 
+    /// 팀 참가신청 거절 API
+    @DeleteMapping("/{teamId}/apply/{userId}/reject")
+    public ResponseEntity<?> rejectApplyTeam(@PathVariable("teamId") Long teamId, @PathVariable("userId") Long userId) {
+        return ResponseEntity.ok().build();
+    }
 }
