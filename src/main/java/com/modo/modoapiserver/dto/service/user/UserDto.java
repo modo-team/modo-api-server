ed0caa7f7b64e398eaaf3bd6afb207ca6ceb44aa
diocto <duhee74@naver.com>
2024-08-18 15:46:51 +0900

    UserDto 변경

--- src/main/java/com/modo/modoapiserver/dto/service/user/UserDto.java
+++ src/main/java/com/modo/modoapiserver/dto/service/user/UserDto.java
@@ -1,14 +1,14 @@
 package com.modo.modoapiserver.dto.service.user;
 
 import com.modo.modoapiserver.enums.Gender;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.util.StringUtils;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserDto {
     private String email;
     private String password;
@@ -18,9 +18,6 @@ public class UserDto {
     private Gender gender;
     private String externalId;
     private String externalType;
-
-    public UserDto() {
-    }
 
     public UserDto(String email, String password, String nickname, Gender gender, String birth) {
         this.email = email;
